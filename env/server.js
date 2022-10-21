3a57578a9974e2dea86c9450816ebe47d5079060
manstarg <sam@Samuels-MacBook-Pro.local>
2022-10-21 16:53:03 +0100

    envfolder

--- env/server.js
+++ env/server.js
@@ -4,6 +4,6 @@ module.exports = ({ env }) => ({
   proxy: true,
   url: env('https://samuelsimon.herokuapp.com/'),
   app: { 
-    keys: env.array('APP_KEYS')
+    keys: env.array(process.env.APP_KEYS)
   },
 });
